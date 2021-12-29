9bf79ab6c6949e0e877996a00cf5bdddc4c38cc5
manolis <argyroi@gmail.com>
2021-12-29 20:46:43 +0200

    first commit

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class H {
+ public static void main(String []args){
+ 
+ Random random = new Random();
+
+ int n = random.nextInt(5);
+ System.out.println(n);
+ }
+}
