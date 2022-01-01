372b980ec10317a22dfead496af663949623c3e0
Manolis Argyros <44981395+ManolisArgyros@users.noreply.github.com>
2022-01-01 14:22:16 +0200

    Add files via upload

---
+++ src/app2.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__) # kataskeyastis
+
+@app.route("/")
+@app.route("/index/")
+@app.route("/<name>")
+def root(name=None):
+    return render_template('index.html',myname=name)
+
+@app.route("/books/")
+def books():
+    
+    with open("mybooks.json") as json_file:
+        jdict = json.load(json_file)
+
+    return render_template("books.html", jdict=jdict)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
